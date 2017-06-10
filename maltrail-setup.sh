407906691fe55471e19c2c959ecb5c6c555583b0
Craig Smith <craigz28@craigsmith.net>
2017-06-10 18:46:34 -0400

    Create maltrail-setup.sh

---
+++ maltrail-setup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+FILE=$1
+IP=$2
+
+# Install dependencies and download Maltrail
+sudo apt -y install git python-pcapy
+git clone https://github.com/stamparm/maltrail.git
+sudo apt -y install schedtool
+
+# Set IP address in server section of maltrail configuration file
+if [ $1 = "server.py" ]; then
+    sed -i '4s/.*/HTTP_ADDRESS '$2'/' $PWD/maltrail/maltrail.conf
+    sed -i '23s/.*/UDP_ADDRESS '$2'/' $PWD/maltrail/maltrail.conf
+    sed -i '26s/.*/UDP_PORT 8337/' $PWD/maltrail/maltrail.conf
+    sed -i '29s/.*/USE_SERVER_UPDATE_TRAILS true/' $PWD/maltrail/maltrail.conf
+fi
+
+# Set IP address in sensor section of maltrail configuration file
+if [ $1 = "sensor.py" ]; then
+    sed -i '69s/.*/LOG_SERVER '$2':8337/' $PWD/maltrail/maltrail.conf
+fi
+
+# Install supervisor to start server.py or sensor.py
+sudo apt -y install supervisor
+
+# Create configuration file for supervisor
+echo "[program:maltrail_script]" >> /etc/supervisor/conf.d/maltrail.conf
+echo "command=sudo python $PWD/maltrail/$FILE" >> /etc/supervisor/conf.d/maltrail.conf
+echo "autostart=true" >> /etc/supervisor/conf.d/maltrail.conf
+echo "autorestart=true" >> /etc/supervisor/conf.d/maltrail.conf
+echo "stderr_logfile=/var/log/maltrail.err.log" >> /etc/supervisor/conf.d/maltrail.conf
+echo "stdout_logfile=/var/log/maltrail.out.log" >> /etc/supervisor/conf.d/maltrail.conf
+
+# Initialize supervisor and start maltrail
+sudo supervisorctl reread
+sudo supervisorctl update
